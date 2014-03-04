2d5a62bf0dde9959ddfae22eb8f76595986cdd3a
Morgan Herlocker <morgan.herlocker@gmail.com>
2014-03-04 10:54:25 -0500

    yw visualizations

---
+++ youngWomen.js
@@ -0,0 +1,21 @@
+var geocolor = require('geocolor'),
+    fs = require('fs')
+
+
+var counties = JSON.parse(fs.readFileSync('./yw-counties-simplified.json'))
+
+var z = 'YoungWomen',
+    numberOfBreaks = 8,
+    colors = ['white', 'red']
+
+// jenks
+jenks = geocolor.jenks(counties, z, numberOfBreaks, colors)
+fs.writeFileSync('./youngWomen_jenks.geojson', JSON.stringify(jenks))
+
+// quantiles
+quantiles = geocolor.quantiles(counties, z, numberOfBreaks, colors)
+fs.writeFileSync('./youngWomen_quantiles.geojson', JSON.stringify(quantiles))
+
+// equal intervals
+equalIntervals = geocolor.equalIntervals(counties, z, numberOfBreaks, colors)
+fs.writeFileSync('./youngWomen_equalInterval.geojson', JSON.stringify(equalIntervals))
---
+++ youngWomenPercent.js
@@ -0,0 +1,21 @@
+var geocolor = require('geocolor'),
+    fs = require('fs')
+
+
+var counties = JSON.parse(fs.readFileSync('./yw-counties-simplified.json'))
+
+var z = 'TotalPop',
+    numberOfBreaks = 8,
+    colors = ['white', 'red']
+
+// jenks
+jenks = geocolor.jenks(counties, z, numberOfBreaks, colors)
+fs.writeFileSync('./population_jenks.geojson', JSON.stringify(jenks))
+
+// quantiles
+quantiles = geocolor.quantiles(counties, z, numberOfBreaks, colors)
+fs.writeFileSync('./population_quantiles.geojson', JSON.stringify(quantiles))
+
+// equal intervals
+equalIntervals = geocolor.equalIntervals(counties, z, numberOfBreaks, colors)
+fs.writeFileSync('./population_equalInterval.geojson', JSON.stringify(equalIntervals))
